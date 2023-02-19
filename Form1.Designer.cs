1fa21e5ed0ca35d939efacf0877bd40bccd586d7
ferdikurnazdm <ferdikurnazdm@gmail.com>
2023-02-19 15:35:49 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace LoginApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_openfolder = new System.Windows.Forms.Button();
+            this.btn_Openlock = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Location = new System.Drawing.Point(12, 114);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(334, 324);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Controls.Add(this.btn_openfolder);
+            this.panel2.Controls.Add(this.btn_Openlock);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Location = new System.Drawing.Point(63, 63);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(334, 324);
+            this.panel2.TabIndex = 1;
+            // 
+            // btn_openfolder
+            // 
+            this.btn_openfolder.Font = new System.Drawing.Font("Nirmala UI Semilight", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_openfolder.Location = new System.Drawing.Point(46, 145);
+            this.btn_openfolder.Name = "btn_openfolder";
+            this.btn_openfolder.Size = new System.Drawing.Size(123, 78);
+            this.btn_openfolder.TabIndex = 3;
+            this.btn_openfolder.Text = "Open Folder";
+            this.btn_openfolder.UseVisualStyleBackColor = true;
+            this.btn_openfolder.Click += new System.EventHandler(this.btn_openfolder_Click);
+            // 
+            // btn_Openlock
+            // 
+            this.btn_Openlock.Font = new System.Drawing.Font("Nirmala UI Semilight", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Openlock.Location = new System.Drawing.Point(178, 145);
+            this.btn_Openlock.Name = "btn_Openlock";
+            this.btn_Openlock.Size = new System.Drawing.Size(122, 78);
+            this.btn_Openlock.TabIndex = 2;
+            this.btn_Openlock.Text = "Open Lock";
+            this.btn_Openlock.UseVisualStyleBackColor = true;
+            this.btn_Openlock.Click += new System.EventHandler(this.btn_Openlock_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI Semilight", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 38);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Password :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(175, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.UseSystemPasswordChar = true;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(12, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(394, 45);
+            this.panel3.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(98, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Saved Folder";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(356, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(418, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_openfolder;
+        private System.Windows.Forms.Button btn_Openlock;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿/*********************************************** Project Information **************************************************************
+ *
+ * project name : saved folder
+ * Company name : ------------
+ * Date         : 19.02.2023
+ * Writed by    : Ferdi Kurnaz
+ * 
+ * *******************************************************************************************************************************
+ * 
+ * Description  : Burada ilk olarak app config dosyasından _passWord ve _filePath key'lerini okuyoruz. Daha sonra password
+ * kontrolü yapıyoruz. pasaport doğru ise klasörün kilidini kaldırıyoruz. kullanıcı yapmak istediği yapıyor daha sonra 
+ * uygulamayı kapatmak istediğinde kullanıcı x işaretine tıkladığı zaman öcde klasör kilitleniyor daha sonra uygulama kapanıyor.
+ * 
+ * *******************************************************************************************************************************
+ * 
+ * Requests     : 
+ * - app config _filePath --> kilitlenecek klasörün dosya dizini (string)
+ * - app config _passWord --> giriş yapmak için gereken şifre (string)
+ * 
+ * *******************************************************************************************************************************/
+using System;
+using System.Configuration;
+using System.IO;
+using System.Security.AccessControl;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace LoginApp
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// hangi field neyi saklıyor.
+        /// _passWord --> kullanıcı şifresini tutar.
+        /// _filePath --> kilitlenecek klasör dizinini tutar.
+        /// _userNema --> hangi Windows oturumu için geçerli olacağını tutar.
+        /// </summary>
+        private static string _passWord;
+        private static string _filePath;
+        private static string _userName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// burada uygulama açılır açılmaz yağılacak işlemler yer almaktadır.
+        /// ek olarak _userName sistem tarafından otomatoik çekilmektedir.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btn_openfolder.Enabled = false;
+            _passWord = ConfigurationSettings.AppSettings["_passWord"];
+            _filePath = ConfigurationSettings.AppSettings["_filePath"];
+            _userName=Environment.UserName;
+        }
+        /// <summary>
+        /// Burada biz password kontrolü yapıyoruz. 
+        /// eğer pasaport doğru ise klasörün kilidini açıyoruz.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Openlock_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text==_passWord)
+            {
+                DirectorySecurity security = Directory.GetAccessControl(_filePath);
+                FileSystemAccessRule rule = new FileSystemAccessRule(_userName, FileSystemRights.FullControl, AccessControlType.Deny);
+                security.RemoveAccessRule(rule);
+                Directory.SetAccessControl(_filePath, security);
+                MessageBox.Show("Unlocked");
+                btn_openfolder.Enabled = true;
+            }
+            else if (textBox1.Text=="")
+            {
+                MessageBox.Show("password is empty!");
+            }
+            else { MessageBox.Show("password is wrong!"); }
+        }
+        /// <summary>
+        /// burada kullanıcı kesin olarak  uygulamayı kapatıcak.
+        /// bizde kullanıcı uygulamayı kapatırken klasörü tekrardan kilitleyeceğiz.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void label1_Click(object sender, EventArgs e)
+        {
+            DirectorySecurity security = Directory.GetAccessControl(_filePath);
+            FileSystemAccessRule rule = new FileSystemAccessRule(_userName, FileSystemRights.FullControl, AccessControlType.Deny);
+            security.AddAccessRule(rule);
+            Directory.SetAccessControl(_filePath, security);
+            MessageBox.Show("Locked");
+            Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// open folder metodunda kullanıcı şifreyi girip klösörün kilidini kaldırdığı zaman.
+        /// open foldeer butonuna tıkladığında klasör dizinine gitmekle uğraşmadan direkt
+        /// karşısına gelecek.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_openfolder_Click(object sender, EventArgs e)
+        {
+            Process.Start(_filePath);
+        }
+    }
+}
